8a651e1121f4ed414bab1bf075e5186825b98d9a
Pocheng <91862467+PochengChen0206@users.noreply.github.com>
2021-12-23 10:44:02 +0800

    1223最新公版更新 之後用PHP包html做 將header、footer、右上的maincon 及 signout功能用require_once代入

---
+++ tpl/foot.inc.php
@@ -0,0 +1,115 @@
+<!-- footer -->
+<div class="footer" style="background: url(./img/footer_bg.jpg) no-repeat;">
+    <div class="footer_container">
+        <div div class="col-lg-12 footer-content">
+            <div class="footer-logo" style="width:20%">
+                <a href=""><img src="./img/logo.png" alt="" style="width: 100%;"></a>
+            </div>
+            <ul class="text-center d-flex flex-column ">
+                <li class="link_list">
+                    <a href="javascript:;">關於我們</a>
+                </li>
+                <li class="link_list">
+                    <a href="javascript:;">最新消息</a>
+                </li>
+                <li class="link_list">
+                    <a href="javascript:;">會員中心</a>
+                </li>
+                <li class="link_list">
+                    <a href="javascript:;">常見問題</a>
+                </li>
+            </ul>
+            <ul class="text-center d-flex flex-column ">
+                <li class="link_list">
+                    <a href="javascript:;">所有商品</a>
+                </li>
+                <li class="link_list" style="font-size:12px;">
+                    <a href="javascript:;">豆單</a>
+                </li>
+                <li class="link_list" style="font-size:12px;">
+                    <a href="javascript:;">嚴選商品</a>
+                </li>
+                <li class="link_list" style="font-size:12px;">
+                    <a href="javascript:;">熱銷排行</a>
+                </li>
+                <li class="link_list" style="font-size:12px;">
+                    <a href="javascript:;">咖啡器材</a>
+                </li>
+            </ul>
+            <ul class="text-center d-flex flex-column ">
+                <li class="link_list">
+                    <a href="javascript:;">課程資訊</a>
+                </li>
+                <li class="link_list" style="font-size:12px;">
+                    <a href="javascript:;">企業包班</a>
+                </li>
+                <li class="link_list" style="font-size:12px;">
+                    <a href="javascript:;">團體課程</a>
+                </li>
+                <li class="link_list" style="font-size:12px;">
+                    <a href="javascript:;">個人課程</a>
+                </li>
+            </ul>
+        </div>
+        <div class="footer-bottom" style="font-size:10px;">
+            <p>版權所有 © minori. All Rights Reserved.</p>
+        </div>
+    </div>
+</div>
+<!-- footer-m -->
+<footer class="footer-m" style="background: url(./img/footer_bg.jpg) no-repeat;">
+    <div class="footer-m-content">
+        <div class="col-md-6 col-12 footer-img pt-5 pb-5">
+            <div class="img-container" style="width: 25%; margin: 0 auto;">
+                <a href=""><img src="./img/logo.png" alt="" style="width: 100%;"></a>
+            </div>
+        </div>
+        <div class="col-md-6 col-12 d-flex text-center pb-5">
+            <div class="col">
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">關於我們</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">最新消息</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">會員中心</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">常見問題</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">所有商品</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white" style="font-size: 12px;">豆單</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white" style="font-size: 12px;">嚴選商品</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white" style="font-size: 12px;">熱銷排行</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white" style="font-size: 12px;">咖啡器材</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">課程資訊</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white" style="font-size: 12px;">企業包班</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white" style="font-size: 12px;">團體課程</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white" style="font-size: 12px;">個人課程</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-m-bottom d-flex justify-content-center" style="color: #fff; font-size: 12px;">
+        <p>版權所有 © minori. All Rights Reserved.</p>
+    </div>
+</footer>
+</div>
+</div>
+</div>
+<!--//////////////////////////////////////// script -->
+<!-- jQ -->
+<script src="./lib/jquery-3.6.0.min.js"></script>
+<!-- jquery UI-->
+<script src="./lib/jquery-ui.min.js"></script>
+<!-- fontawesome -->
+<script src="./lib/awesome.all.min.js"></script>
+<!-- bootstrap@4.6 -->
+<script src="./lib/bootstrap.bundle.min.js"></script>
+<!-- js -->
+<script src="./js/index.js"></script>
+</body>
+
+</html>
